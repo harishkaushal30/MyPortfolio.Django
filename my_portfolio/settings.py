01ce923d1ad2dd9dfc72ac1ed46171f7d3324160
Harish <harishkaushal30@gmail.com>
2020-05-25 12:13:32 +0530

    added whitenoise

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,10 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-STATIC_ROOT = "/home/site/wwwroot/static/"
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+#     "D:\\home\\site\\wwwroot\\static\\",
+# )
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
