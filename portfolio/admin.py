1187172e8036ae592424047610129219c37d01de
Harish Kaushal <harishkaushal30@gmail.com>
2020-06-09 23:01:38 +0530

    Fixing .gitignore

--- portfolio/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from portfolio.models import MyInfo, Contact
-
-admin.site.register(MyInfo)
-admin.site.register(Contact)
--- portfolio/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import  path
-from portfolio import views
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('error/', views.error, name='error'),
-]
